921811c122077a912a476ee1d19d0d5fc61f9dfc
miss.daee <florchu161@gmail.com>
2021-08-24 14:17:07 -0300

    first commit

---
+++ json.js
@@ -0,0 +1,27 @@
+//1) 
+//a) Crear un objeto JSON con información de un usuario: nombre o apodo, instrumento/superpoder/profesión (a elección de ustedes), edad.
+
+const usuario= {
+    "nombre": "Leia",
+    "apellido": "Organa",
+    "edad": 19,
+    "profesion": "senadora",
+}
+
+// //b) Mostrar un mensaje en el HTML con la información del usuario
+// console.log(usuario)
+
+
+document.write(JSON.stringify(usuario))  // te imprime tal cual el array en pantalla
+
+// c) Agregar un botón que da la opción de modificar cualquiera de los tres datos.
+
+
+
+
+// d) Agregar un botón que permita generar un nuevo usuario.
+// e) Agregar un botón que muestre todos los usuarios.
+// f) Agregar un botón que permita borrar todos los usuarios.
+// g) Agregar un botón que muestre un usuario por nombre.
+// h) Agregar un botón que elimine un usuario por nombre.
+// i) Agregar un botón que permita agregar nuevos datos a un usuario elegido.
